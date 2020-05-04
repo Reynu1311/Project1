1e81e80030d20cd7b231dd56c6eb861cf814bbc3
Reynu1311 <renuviji13@gmail.com>
2020-05-05 00:36:36 +0530

    Coding updated

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,27 @@
+package org.phone;
+
+public class PhoneInfo {
+	public void phoneName() {
+		System.out.println("Phone name is Iphone11");
+	}
+	public void phoneImeino() {
+		System.out.println("Phone imei id is 123456789");
+	}
+	public void phoneCamera() {
+		System.out.println("Phone camera is 18megapixel");
+	}
+	public void phoneStorage() {
+		System.out.println("Phone storage is 128GB");
+	}
+	public void phoneOs() {
+		System.out.println("Phone operating system is iOS");
+	}
+	public static void main(String[] args) {
+		PhoneInfo p=new PhoneInfo();
+		p.phoneName();
+		p.phoneImeino();
+		p.phoneCamera();
+		p.phoneStorage();
+		p.phoneOs();
+	}
+}
